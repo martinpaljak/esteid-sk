a285335d2359b21f8a7af4583d8122993e5c1ce4
Martin Paljak <martin@martinpaljak.net>
2021-04-07 17:56:27 +0300

    Add missing depend

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module org.esteid.sk {
     requires org.slf4j;
     requires org.bouncycastle.pkix;
+    requires org.bouncycastle.provider;
     requires java.naming;
-    
+
     exports org.esteid.sk;
 }
